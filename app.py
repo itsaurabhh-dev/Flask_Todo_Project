42d423d3199d6a60c81e37a9e3055ba1d4a12ef0
itsaurabhh <itsaurabhh@gmail.com>
2026-08-22 17:48:49 +0530

    Added submit todo api

--- app.py
+++ app.py
@@ -59,6 +59,17 @@ def home():
 def success():
     return render_template("success.html")            
 
+@app.route("/submittodoitem", methods=["POST"])
+def submit():
+    item = {
+        "itemName": request.form["itemName"],
+        "itemDescription": request.form["itemDescription"]
+    }
+
+    collection.insert_one(item)
+
+    return "Saved Successfully"
+
 
 if __name__ =='__main__':
     app.run(debug=True)
